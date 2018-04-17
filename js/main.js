3c13089f00d6b163264fce15b76c1fbc4bd33869
sumansaurabh <ss.sumansaurabh92@gmail.com>
2018-04-18 04:05:35 +0530

    Fixed HTMl

--- js/main.js
+++ js/main.js
@@ -2,33 +2,29 @@ $(document).ready(function(){
     
     $('#subbut').click(function(){
     	var subject = $("#subjectid").val();
-    
     	var msg = $("#messageid").val();
-    	var data={"subject":subject, "message":msg};
+      var emailid= $("#emailid").val();
+    	var data={"subject":subject, "message":msg, "email": emailid};
+      data = {"subject": "Test subject", "message": "test message", "email": "suman@q.com"};
+      console.log(data);
     
       
         $.ajax({
-            url: '/post',
+            url: '/sendMail',
             type: 'POST',
             data: data,
             dataType: 'json',
             success: function(data,status){
-              if(data.login_url)
-              {
-                alert("Please Sign In to 'Google' account before sending the message.");
-                window.location=data.login_url;
-              }
-              else
-              {
-                $('#contact_form').html("<div id='message'></div>");
+              
+              $('#contact_form').html("<div id='message'></div>");
               $('#message').html("<h2>Message Submitted</h2>")
-              .append("<p>I will be in touch soon.</p>")
-              .hide()
-              .fadeIn(1500, function() {
-                $('#message').append("");
-              });
+                .append("<p>I will be in touch soon.</p>")
+                .hide()
+                .fadeIn(1500, function() {
+                  $('#message').append("");
+                });
                 //alert("Message was Successfully sent.");
-              }
+              
             }
        });
        return false;
